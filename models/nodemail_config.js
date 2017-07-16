7b40f4f77c0d4c847213038f7f7412a5368e5082
paulson <paulsontiti@gmail.com>
2017-07-16 12:04:50 +0100

    init

---
+++ models/nodemail_config.js
@@ -0,0 +1,8 @@
+﻿/**Add all configuration settings here*/
+module.exports = {
+    mailUser: "titidprogrammer@gmail.com",
+    mailPass: "titile1987",
+    clientId: "56442406317-4nbq6e4mu87sgk9b8um051ulego19ais.apps.googleusercontent.com",
+    clientSecret: "_MivzchRHbls662jupT2AsUD",
+refreshToken: "1/MI7MPGQ-l4BXpMA-hMB4BmHwtcHnaS0AjhWRtuynmlg"
+}
